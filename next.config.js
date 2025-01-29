f990bfe57b5963466d094e07619621f9abcd611e
AbdelrhmnaMohmaed <boodyhope@gmail.com>
2025-01-29 15:02:10 +0200

    new

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/audio-chart", // Updated to match your repository name
-};
-
-module.exports = nextConfig;
---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/audio-chart", // Updated to match your repository name
+};
+
+export default nextConfig;
